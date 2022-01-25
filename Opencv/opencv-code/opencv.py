81f5c26b6bae491c3e4f751ea52a19c504234ffe
C#EXAMPLE <794919561@qq.com>
2022-01-25 17:21:20 +0800

    OpenCV-0125

--- Opencv/opencv-code/opencv.py
+++ Opencv/opencv-code/opencv.py
@@ -604,33 +604,35 @@
 # -*- coding: utf-8 -*-
 # import cv2
 # import numpy as np
-# img = cv2.imread('E:\\Deep Learning\\DeepLearning\\Opencv\\Opencv.jpg')
+# img = cv2.imread('E:\\Deep Learning\\DeepLearning\\Opencv\\Opencv.jpg',0)
 # rows,cols=img.shape
 # # 这里的第一个参数为旋转中心，第二个为旋转角度，第三个为旋转后的缩放因子
 # # 可以通过设置旋转中心，缩放因子，以及窗口大小来防止旋转后超出边界的问题
 # M=cv2.getRotationMatrix2D((cols/2,rows/2),45,0.6)
 # # 第三个参数是输出图像的尺寸中心
 # dst=cv2.warpAffine(img,M,(2*cols,2*rows))
+# cv2.imwrite('E:\\Deep Learning\\DeepLearning\\Opencv\\Opencv_Rotation.jpg',dst)
 # while(1):
 #     cv2.imshow('img',dst)
 #     if cv2.waitKey(1) & 0xFF == 27:
 #         break
 # cv2.destroyAllWindows()
 
-# 仿射变换
+
+# 14.4 仿射变换
 # 为了创建这个矩阵我们需要从原图像中找到三个点以及他们在输出图像中的位置。然后cv2.getAffineTransform 会创建一个 2x3 的矩阵，最后这个矩阵会被传给函数 cv2.warpAffine;
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
-# img = cv2.imread('D:/software/DL information/Opencv/Opencv.png')
-# rows,cols,ch=img.shape
-# pts1=np.float32([[50,50],[200,50],[50,200]])
-# pts2=np.float32([[10,100],[200,50],[100,250]])
-# M=cv2.getAffineTransform(pts1,pts2)
-# dst=cv2.warpAffine(img,M,(cols,rows))
-# plt.subplot(121,plt.imshow(img),plt.title('Input'))
-# plt.subplot(121,plt.imshow(img),plt.title('Output'))
-# plt.show()
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv2.imread('D:\\DL information\\Opencv\\VR.jpg')
+rows,cols,ch=img.shape
+pts1=np.float32([[50,50],[200,50],[50,200]])
+pts2=np.float32([[10,100],[200,50],[100,250]])
+M=cv2.getAffineTransform(pts1,pts2)
+dst=cv2.warpAffine(img,M,(cols,rows))
+plt.subplot(121,plt.imshow(img),plt.title('Input'))
+plt.subplot(121,plt.imshow(img),plt.title('Output'))
+plt.show()
 
 # 14.5 透视变换
 # 对于视角变换，我们需要一个 3x3 变换矩阵。在变换前后直线还是直线。
